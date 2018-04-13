b538c14113964e472020ed77369bdaf4d542dd55
ivan <i.honcharenko@lerpal.com>
2018-04-13 20:07:43 +0300

    add login form:

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import blog
+from app.forms import LoginForm
+
 
 @blog.route('/')
 @blog.route('/index')
 def index():
-    user = {'username':'Ivan'}
+    user = {'username': 'Ivan'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,6 +19,17 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
 @blog.route('/secret')
 def secret():
     return "not so smart"
+
+
+@blog.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
